fbf87be2925e0998a11bed4b40388d6a1bb11925
EOussama <oussama_gtacj@live.fr>
2023-03-14 16:54:07 +0100

    Initialization

---
+++ index.js
@@ -0,0 +1,35 @@
+const API_PORT = process.env.PORT || 3000;
+// const REDIS_HOST = process.env.REDIS_HOST || 'redis';
+// const REDIS_PORT = process.env.REDIS_PORT || 6379;
+// // const REDIS_URL = process.env.REDIS_URL
+// const REDIS_URL = `${REDIS_HOST}://redis:${REDIS_PORT}`;
+
+import express from 'express';
+import { createClient } from 'redis';
+
+(async () => {
+  const app = express();
+  const client = createClient();
+
+  client.on("error", (err) => {
+    console.log("Error " + err);
+  });
+
+  app.get('/', async (req, res) => {
+    try {
+      const count = await client.incr('counter');
+      res.send(`Counter: ${count}`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error incrementing counter');
+    }
+  });
+
+  app.listen(API_PORT, async () => {
+    await client.connect();
+    console.log(`App listening on port ${API_PORT}`);
+  });
+})();
+
+
+
