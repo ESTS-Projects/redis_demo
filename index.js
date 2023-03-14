37a090e70fc59c3e08b584df2e613de37b6b9433
EOussama <oussama_gtacj@live.fr>
2023-03-14 17:12:37 +0100

    Config

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const API_PORT = process.env.PORT || 3000;
-// const REDIS_HOST = process.env.REDIS_HOST || 'redis';
-// const REDIS_PORT = process.env.REDIS_PORT || 6379;
-// // const REDIS_URL = process.env.REDIS_URL
-// const REDIS_URL = `${REDIS_HOST}://redis:${REDIS_PORT}`;
+const REDIS_HOST = process.env.REDIS_HOST || 'redis';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_URL = `redis://${REDIS_HOST}:${REDIS_PORT}`;
 
 import express from 'express';
 import { createClient } from 'redis';
 
 (async () => {
   const app = express();
-  const client = createClient();
+  const client = createClient({ url: REDIS_URL });
 
   client.on("error", (err) => {
     console.log("Error " + err);
